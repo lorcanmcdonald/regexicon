9e280036fb9300a41cf42149a973b7d447dc5c7c
Lorcan McDonald <lorcan@lorcanmcdonald.com>
2017-06-27 16:34:33 +0100

    Show examples only when there are no results

--- src/Matching/Server/Views.hs
+++ src/Matching/Server/Views.hs
@@ -15,7 +15,6 @@ landingPage results = renderHtml . H.docTypeHtml $ do
     H.head $ do
         H.title "Regular Expressions"
         H.meta ! A.name "viewport" ! A.content "width=device-width, initial-scale=1"
-        H.script ! src "/js/bundle.js" $ ""
         (H.link ! rel "stylesheet") ! href "style/style.css"
         (H.link ! rel "stylesheet")
           ! href "style/mobile.css"
@@ -26,12 +25,12 @@ landingPage results = renderHtml . H.docTypeHtml $ do
         H.form ! A.method "GET" ! A.action "/" $ do
           H.input ! A.name "q" ! A.id "q" ! A.autofocus "" ! A.placeholder "[0-9a-f]{32}"
           H.button "🔍"
-        H.div ! A.class_ "examples" $do
-          H.span "e.g.: "
-          H.ul $ do
-            H.li $ H.a ! A.href "/?q=[0-9a-f]{32}" $ "[0-9a-f]{32}"
-            H.li $ H.a ! A.href "/?q=[\128512-\128522]+" $ "[\128512-\128522]+"
-        H.ul ! A.class_ "results" $ mapM_ (H.li . text) $ toResults results
+        case results of
+          (RegexResults _) = resultList
+          _ = examples
+
+
+        H.script ! src "/js/bundle.js" $ ""
         H.script
           "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-100307936-1', 'auto');ga('send', 'pageview');"
   where
@@ -39,6 +38,15 @@ landingPage results = renderHtml . H.docTypeHtml $ do
   toResults (RegexResults candidates) = toS <$> candidates
   toResults RegexTimeout = ["Regular expression too complex to calculate"]
   toResults (RegexParseFailure _) = ["Could not parse regular expression"]
+
+  examples =
+    H.div ! A.class_ "examples" $ do
+      H.span "e.g.: "
+      H.ul $ do
+        H.li $ H.a ! A.href "/?q=[0-9a-f]{32}" $ "[0-9a-f]{32}"
+        H.li $ H.a ! A.href "/?q=[\128512-\128522]+" $ "[\128512-\128522]+"
+  resultList =
+    H.ul ! A.class_ "results" $ mapM_ (H.li . text) $ toResults results
 
 selectMatches :: Int -> ByteString -> IO RegexResults
 selectMatches n = matches n . toS
