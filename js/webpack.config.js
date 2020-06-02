b05c2d4e9c2464d01c16ec95bb438dc18a074ac4
Lorcan McDonald <lorcan@lorcanmcdonald.com>
2020-06-02 11:33:31 +0100

    Apply `prettier` formatting to webpack.config.js

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -4,37 +4,37 @@ const clc = require("cli-color"),
 
 const config = {
   entry: {
-    app: path.resolve(__dirname, "./client.jsx")
+    app: path.resolve(__dirname, "./client.jsx"),
   },
   resolve: {
     alias: {
       react: "preact/compat",
-      "react-dom": "preact/compat"
+      "react-dom": "preact/compat",
     },
     extensions: [".js", ".jsx"],
-    modules: [__dirname, path.resolve(__dirname, "node_modules")]
+    modules: [__dirname, path.resolve(__dirname, "node_modules")],
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   devtool: "hidden-source-map",
   module: {
     rules: [
       { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
       { test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"] },
-      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] }
-    ]
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+    ],
   },
   plugins: [
     new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": '"production"'
+      "process.env.NODE_ENV": '"production"',
     }),
     new webpack.DefinePlugin({
-      "globalConfig.env": '"production"'
-    })
-  ]
+      "globalConfig.env": '"production"',
+    }),
+  ],
 };
 
 module.exports = config;
