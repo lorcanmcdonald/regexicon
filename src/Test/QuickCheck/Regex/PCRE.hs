a06c0d7c9b95e2818c67e233d2c858ad98917f44
Lorcan McDonald <lorcan@lorcanmcdonald.com>
2020-06-09 11:59:35 +0100

    Create Exemplify type class
    `Exemplify` extracts the behaviour of `matching` over all the contained
    types in `Regex`

--- src/Test/QuickCheck/Regex/PCRE.hs
+++ src/Test/QuickCheck/Regex/PCRE.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Test.QuickCheck.Regex.PCRE
@@ -36,103 +37,109 @@ import Test.QuickCheck.Regex.PCRE.Render
 import Test.QuickCheck.Regex.PCRE.Types
 
 matching :: Regex -> Gen String
-matching (Regex reChar) = matchingPattern reChar
-matching (StartOfString reChar) = matchingPattern reChar
-matching (EndOfString reChar) = matchingPattern reChar
-matching (StartAndEndOfString reChar) = matchingPattern reChar
+matching = examples
 
-matchingPattern :: Pattern -> Gen String
-matchingPattern (Alternative x xs) = oneof . map charList $ x : xs
+class Exemplify a where
+  examples :: a -> Gen String
 
-charList :: [RegexCharacter] -> Gen String
-charList = fmap concat . traverse matchingChar
+instance Exemplify Regex where
+  examples (Regex reChar) = examples reChar
+  examples (StartOfString reChar) = examples reChar
+  examples (EndOfString reChar) = examples reChar
+  examples (StartAndEndOfString reChar) = examples reChar
 
-matchingChar :: RegexCharacter -> Gen String
-matchingChar (Quant q) = matchingQuantifiable q
-matchingChar (Meta m) = matchingMeta m
-matchingChar (Quoted s) = pure s
+instance Exemplify Pattern where
+  examples (Alternative x xs) = oneof . map examples $ x : xs
 
-matchingQuantifiable :: Quantifiable -> Gen String
-matchingQuantifiable AnyCharacter = fmap (: "") regexChars
-matchingQuantifiable (AmbiguousNumberSequence _) = error "Should not generate an AmbiguousNumberSequence"
-matchingQuantifiable (Backslash b) = matchingBackslash b
-matchingQuantifiable (BackReference _ p) = matchingPattern p
-matchingQuantifiable (Character char) = elements [[char]]
-matchingQuantifiable (CharacterClass firstChar chars) =
-  oneof $ matchingCharacterClassCharacters <$> (firstChar : chars)
-matchingQuantifiable (NegatedCharacterClass firstChar chars) =
-  (: []) <$> regexChars
-    `suchThat` (\a -> (not . any (inCharacterClassCharacter a)) (firstChar : chars))
-matchingQuantifiable (Subpattern re) = matchingPattern re
+instance Exemplify [RegexCharacter] where
+  examples = fmap concat . traverse examples
 
-matchingMeta :: MetaCharacter -> Gen String
-matchingMeta (ZeroOrMore q) = fmap concat . listOf $ matchingQuantifiable q
-matchingMeta (OneOrMore q) = fmap concat . listOf1 $ matchingQuantifiable q
-matchingMeta (MinMax q r) = do
-  let (a, b) = extractPositiveRange r
-  k <- choose (a, b)
-  fmap concat . vectorOf k $ matchingQuantifiable q
+instance Exemplify RegexCharacter where
+  examples (Quant q) = examples q
+  examples (Meta m) = examples m
+  examples (Quoted s) = pure s
 
-matchingBackslash :: BackslashSequence -> Gen String
-matchingBackslash (Nonalphanumeric c) = pure (c : "")
-matchingBackslash BackslashChar = pure "\\"
-matchingBackslash Caret = pure "\\^"
-matchingBackslash Dollar = pure "\\$"
-matchingBackslash Dot = pure "\\."
-matchingBackslash OpenSquareBracket = pure "\\["
-matchingBackslash Pipe = pure "\\|"
-matchingBackslash OpenParens = pure "\\("
-matchingBackslash CloseParens = pure "\\)"
-matchingBackslash QuestionMark = pure "\\?"
-matchingBackslash Asterisk = pure "\\*"
-matchingBackslash Plus = pure "\\+"
-matchingBackslash OpenBrace = pure "\\{"
-matchingBackslash Hyphen = pure "\\-"
-matchingBackslash CloseSquareBracket = pure "\\]"
-matchingBackslash NonprintingAlarm = pure "\\a"
-matchingBackslash (NonprintingCtrlx c) = pure ("\\c" <> [c])
-matchingBackslash NonprintingEscape = pure "\\e"
-matchingBackslash NonprintingFormFeed = pure "\\f"
-matchingBackslash NonprintingLineFeed = pure "\\n"
-matchingBackslash NonprintingCarriageReturn = pure "\\r"
-matchingBackslash NonprintingTab = pure "\\t"
-matchingBackslash (NonprintingOctalCodeZero c) = pure ("\\0" <> showOct c "")
-matchingBackslash (NonprintingOctalCode c) = pure ("\\" <> showOct c "")
-matchingBackslash (NonprintingOctalCodeBraces c) = pure ("\\o{" <> showOct c "" <> "}")
-matchingBackslash NonprintingHexZero = pure "\\x"
-matchingBackslash (NonprintingHexCode c) = pure ("\\x" <> showHex c "")
-matchingBackslash (NonprintingHexCodeBraces c) = pure ("\\x{" <> showHex c "" <> "}")
-matchingBackslash Digit =
-  oneof
-    [ pure "0",
-      pure "1",
-      pure "2",
-      pure "3",
-      pure "4",
-      pure "5",
-      pure "6",
-      pure "7",
-      pure "8",
-      pure "9"
-    ]
-matchingBackslash NonDigit =
-  fmap (: "") (arbitraryASCIIChar `suchThat` (not . isDigit))
-matchingBackslash HorizontalWhiteSpace =
-  (: "") <$> oneof (map return horizontalSpace)
-matchingBackslash NotHorizontalWhiteSpace =
-  fmap (: "") arbitraryASCIIChar `suchThat` (`notElem` map return horizontalSpace)
-matchingBackslash VerticalWhiteSpace =
-  (: "") <$> oneof (map return verticalSpace)
-matchingBackslash NotVerticalWhiteSpace =
-  fmap (: "") arbitraryASCIIChar `suchThat` (`notElem` map return verticalSpace)
-matchingBackslash WhiteSpace =
-  (: "") <$> oneof (map return whiteSpace)
-matchingBackslash NotWhiteSpace =
-  fmap (: "") arbitraryASCIIChar `suchThat` (`notElem` map return whiteSpace)
-matchingBackslash WordCharacter =
-  (: "") <$> oneof (map return wordChar)
-matchingBackslash NonWordCharacter =
-  fmap (: "") arbitraryASCIIChar `suchThat` (`notElem` map return wordChar)
+instance Exemplify Quantifiable where
+  examples AnyCharacter = fmap (: "") regexChars
+  examples (AmbiguousNumberSequence _) = error "Should not generate an AmbiguousNumberSequence"
+  examples (Backslash b) = examples b
+  examples (BackReference _ p) = examples p
+  examples (Character char) = elements [[char]]
+  examples (CharacterClass firstChar chars) =
+    oneof $ examples <$> (firstChar : chars)
+  examples (NegatedCharacterClass firstChar chars) =
+    (: []) <$> regexChars
+      `suchThat` (\a -> (not . any (inCharacterClassCharacter a)) (firstChar : chars))
+  examples (Subpattern re) = examples re
+
+instance Exemplify MetaCharacter where
+  examples (ZeroOrMore q) = fmap concat . listOf $ examples q
+  examples (OneOrMore q) = fmap concat . listOf1 $ examples q
+  examples (MinMax q r) = do
+    let (a, b) = extractPositiveRange r
+    k <- choose (a, b)
+    fmap concat . vectorOf k $ examples q
+
+instance Exemplify BackslashSequence where
+  examples (Nonalphanumeric c) = pure (c : "")
+  examples BackslashChar = pure "\\"
+  examples Caret = pure "\\^"
+  examples Dollar = pure "\\$"
+  examples Dot = pure "\\."
+  examples OpenSquareBracket = pure "\\["
+  examples Pipe = pure "\\|"
+  examples OpenParens = pure "\\("
+  examples CloseParens = pure "\\)"
+  examples QuestionMark = pure "\\?"
+  examples Asterisk = pure "\\*"
+  examples Plus = pure "\\+"
+  examples OpenBrace = pure "\\{"
+  examples Hyphen = pure "\\-"
+  examples CloseSquareBracket = pure "\\]"
+  examples NonprintingAlarm = pure "\\a"
+  examples (NonprintingCtrlx c) = pure ("\\c" <> [c])
+  examples NonprintingEscape = pure "\\e"
+  examples NonprintingFormFeed = pure "\\f"
+  examples NonprintingLineFeed = pure "\\n"
+  examples NonprintingCarriageReturn = pure "\\r"
+  examples NonprintingTab = pure "\\t"
+  examples (NonprintingOctalCodeZero c) = pure ("\\0" <> showOct c "")
+  examples (NonprintingOctalCode c) = pure ("\\" <> showOct c "")
+  examples (NonprintingOctalCodeBraces c) = pure ("\\o{" <> showOct c "" <> "}")
+  examples NonprintingHexZero = pure "\\x"
+  examples (NonprintingHexCode c) = pure ("\\x" <> showHex c "")
+  examples (NonprintingHexCodeBraces c) = pure ("\\x{" <> showHex c "" <> "}")
+  examples Digit =
+    oneof
+      [ pure "0",
+        pure "1",
+        pure "2",
+        pure "3",
+        pure "4",
+        pure "5",
+        pure "6",
+        pure "7",
+        pure "8",
+        pure "9"
+      ]
+  examples NonDigit =
+    fmap (: "") (arbitraryASCIIChar `suchThat` (not . isDigit))
+  examples HorizontalWhiteSpace =
+    (: "") <$> oneof (map return horizontalSpace)
+  examples NotHorizontalWhiteSpace =
+    fmap (: "") arbitraryASCIIChar `suchThat` (`notElem` map return horizontalSpace)
+  examples VerticalWhiteSpace =
+    (: "") <$> oneof (map return verticalSpace)
+  examples NotVerticalWhiteSpace =
+    fmap (: "") arbitraryASCIIChar `suchThat` (`notElem` map return verticalSpace)
+  examples WhiteSpace =
+    (: "") <$> oneof (map return whiteSpace)
+  examples NotWhiteSpace =
+    fmap (: "") arbitraryASCIIChar `suchThat` (`notElem` map return whiteSpace)
+  examples WordCharacter =
+    (: "") <$> oneof (map return wordChar)
+  examples NonWordCharacter =
+    fmap (: "") arbitraryASCIIChar `suchThat` (`notElem` map return wordChar)
 
 horizontalSpace :: String
 horizontalSpace =
@@ -174,8 +181,8 @@ whiteSpace = horizontalSpace <> verticalSpace
 wordChar :: String
 wordChar = ['0' .. '9'] <> ['A' .. 'Z'] <> ['a' .. 'z']
 
-matchingCharacterClassCharacters :: CharacterClassCharacter -> Gen String
-matchingCharacterClassCharacters (ClassLiteral c) = elements [[c]]
-matchingCharacterClassCharacters (ClassRange r) =
-  fmap (: "") . choose $ extractRange r
-matchingCharacterClassCharacters (QuotedClassLiterals c) = elements [c]
+instance Exemplify CharacterClassCharacter where
+  examples (ClassLiteral c) = elements [[c]]
+  examples (ClassRange r) =
+    fmap (: "") . choose $ extractRange r
+  examples (QuotedClassLiterals c) = elements [c]
